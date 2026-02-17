d7231f949a1b4bc6004591680ec76443f24d0ad0
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-02-17 20:49:36 +0530

    basic

---
+++ 1-Basics/2variable.py
@@ -0,0 +1,32 @@
+# how to decalre variable name
+myvar = 1
+my_var = 1
+myVar= 1
+
+
+x = 5
+print(x)
+print(type(x))
+
+
+
+#global variable
+
+a = "Akash"
+
+def name():
+    print("Uma" + a)
+
+name()
+
+
+
+def code():
+    global b
+    b = "python"
+
+    print(b + " " + "is a coding language")
+
+
+code()
+
---
+++ 4casting.py
@@ -0,0 +1,8 @@
+x = float(4)
+y = int(4.45)
+z = str(34)
+
+print(x,"",y,"",z)
+print(type(x))
+print(type(y))
+print(type(z))
