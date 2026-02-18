498f2bac5cf2e57044e810ed44f9bdf90c3b0594
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-02-18 20:47:36 +0530

    if,elif,else

---
+++ 5-If,else,elif/if.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+if a > b:
+    print("a is big")
+
+
+if a<b :
+    print("b is big")
+
+
+
+is_logged_in = True
+
+if is_logged_in:
+    print("Good")
---
+++ 5-If,else,elif/nested.py
@@ -0,0 +1,11 @@
+age = 2
+is_Licences=True
+
+if age > 18:
+    if is_Licences:
+        print("you can drive")
+    else:
+        print("you cannot drive")
+
+else:
+    print("you are to young ")
