8570f7d5481a109cc848619a24909ba1fa04cfb4
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-02-18 21:02:13 +0530

    loops

---
+++ 6-Loops/for.py
@@ -0,0 +1,27 @@
+# for x in "banana":
+#     print(x)
+
+
+# myList = [1,2,3,4,5,6,7,8,9,10]
+# for i in myList:
+#     print(i)
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+# n = 19
+
+# for i in range(1,11):
+#     print(n,"x",i,"=",n*i)
+
+
+# nested for loop
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+
+for x in a:
+    for y in b:
+        print(x,y)
---
+++ 6-Loops/while.py
@@ -0,0 +1,33 @@
+i = 1
+
+# syntax
+# while i < 10:
+#     # print(i)
+#     i+=1
+
+
+# break
+# while i < 10:
+#     print(i)
+#     if i == 6:
+#         break
+#     i+=1
+
+
+
+# continue
+
+# while i < 10:
+#     i+=1
+#     if i == 3:
+#         continue
+#     print(i)
+
+
+# using else
+
+while i <6:
+    print(i)
+    i+=1
+else:
+    print("end")
