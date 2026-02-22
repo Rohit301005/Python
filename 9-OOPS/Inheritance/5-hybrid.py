3aff57d8dd1795f21581007c36e4090040369cf7
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-02-22 22:22:23 +0530

    Inheritance

---
+++ 9-OOPS/Inheritance/5-hybrid.py
@@ -0,0 +1,24 @@
+class A:
+    def show_A(self):
+        print("Class A")
+
+# Hierarchical inheritance
+class B(A):
+    def show_B(self):
+        print("Class B")
+
+class C(A):
+    def show_C(self):
+        print("Class C")
+
+# Multiple inheritance (combining B and C)
+class D(B, C):
+    def show_D(self):
+        print("Class D")
+
+obj = D()
+
+obj.show_A()
+obj.show_B()
+obj.show_C()
+obj.show_D()
