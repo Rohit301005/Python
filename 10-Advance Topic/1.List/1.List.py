bb3f04d6188a83420df7d1d200d88bfdbd450373
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-03-04 23:47:36 +0530

    advance

---
+++ 10-Advance Topic/1.List/1.List.py
@@ -0,0 +1,10 @@
+# List : order,mutable,allow duplicate element
+
+myList1 = ["apple","banana",'mango']
+print(myList1)
+
+myList2 = []
+print(myList2)
+
+myList3 = [5,True,"apple",'banana','mango',[myList1],'apple']
+print(myList3)
---
+++ 10-Advance Topic/1.List/2.index.py
@@ -0,0 +1,21 @@
+myList1 = ["apple","banana",'mango']
+print(myList1)
+
+# item = myList1[0]
+# print(item)
+# item = myList1[2]
+# print(item)
+# item = myList1[1]
+# print(item)
+
+
+
+# item = myList1[-1]
+# print(item)
+# item = myList1[-2]
+# print(item)
+# item = myList1[-3]
+# print(item)
+
+for i in myList1:
+    print(i)
